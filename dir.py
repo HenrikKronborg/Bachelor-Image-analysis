ebe4499c2038db3a6fb4dc6a4967ecf01920b583
HenrikKronborg <henrik.kronborg@hiof.no>
2018-02-14 11:32:59 +0100

    Hent Jonas

--- dir.py
+++ dir.py
@@ -1,32 +1,63 @@
 #!/urs/bin/python
 import os
+import time
 from datetime import datetime
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
 #from time import gmtime, strftime
 
+
 deteksjonArray = []
-
-
+videoQueue = Queue()
+test = 0
+test2=0
 
 ##Gjør noe i oppstarten èn gang
 #TODO: def(?) innstillinger, startparametere(?)
 
-#TODO: def
+#TODO: def threadStart():
 ##Egen prosess/tråd som konstant filmer i intervaller, lagrer nye mapper for frames osv(?)
 #TODO: Start tråd som kjører independent, kanskje bare på kveldstid(?)
 ## obs, filstier *kan* variere
 ##Lag mappe for neste intervall
-i = datetime.now()
-#_ TODO: Legge til disse i en kø? First in, first out? Array? Noe som holder styr på neste video som kan analyseres ( husk X mappe må være klar->ha alle frames lagd/filmet ferdig)
-CurrentVideo = str(i.strftime('%Y_%m_%d_%H:%M:%S'))
-os.system("mkdir /home/nvidia/Bachelor/Frames/" + CurrentVideo) #<- Navnet blir brukt i Henrik-funk
-#TODO: Film
+while(test < 3):
+	i = datetime.now()
+	#_ TODO: Legge til disse i en kø? First in, first out? Array? Noe som holder styr på neste video som kan analyseres ( husk X mappe må være klar->ha alle frames lagd/filmet ferdig)
+	CurrentVideo = str(i.strftime('%Y_%m_%d_%H:%M:%S'))
+	#print(CurrentVideo)
+	videoQueue.enqueue(CurrentVideo)
+	time.sleep(1.1)# For test
+	test += 1
+#Skriv ut køen
+#while test2 < videoQueue.size():
+#	print(videoQueue.dequeue())
+
+	os.system("mkdir /home/nvidia/Bachelor/Frames/" + CurrentVideo) #<- Navnet blir brukt i Henrik-funk
+#TODO: Film --> Henrik_Code()
 
 
 
 ##Hvis det er to video-intervaller i Videoer-mappen; start analyse på første//Kan byttes ut med noe annet så lenge vi ikke begynner før et helt intervall er ferdig.
-if len(os.listdir('/home/nvidia/Bachelor/Videoer')) > 1:
+#if len(os.listdir('/home/nvidia/Bachelor/Videoer')) > 1:
 	#todo: for			
 		#if end=".mp4"
+
+		
+		
 
 '''
 for video in os.listdir('/home/bachelor/Git/Bachelor/raw'):
--- henrik/test.sh
+++ henrik/test.sh
@@ -1,7 +1,7 @@
 #gst-launch-1.0 tcamsrc serial=4810628 ! video/x-bayer,format=bggr,width=1024,height=768 ! capssetter join=false caps="video/x-bayer,format=gbrg" ! bayer2rgb ! tee name=t t. ! queue ! videoscale ! video/x-raw,framerate=30/1 ! videoconvert ! ximagesink t. ! queue ! videorate ! video/x-raw,framerate=1/1 ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg
 
 
-gst-launch-1.0 tcamsrc num-buffers=40 serial=4810628 ! video/x-bayer,format=bggr,width=1024,height=768 ! capssetter join=false caps="video/x-bayer,format=gbrg" ! bayer2rgb ! tee name=t t. ! queue ! videoscale ! video/x-raw,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=bilder/faen.mp4 t. ! queue ! videorate ! video/x-raw,framerate=1/1 ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg
+#gst-launch-1.0 tcamsrc num-buffers=40 serial=4810628 ! video/x-bayer,format=bggr,width=1024,height=768 ! capssetter join=false caps="video/x-bayer,format=gbrg" ! bayer2rgb ! tee name=t t. ! queue ! videoscale ! video/x-raw,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=bilder/faen.mp4 t. ! queue ! videorate ! video/x-raw,framerate=1/1 ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg
 
 
 #gst-launch-1.0 -e tcamsrc num-buffers=120 serial=4810628 ! tee name=t t. ! queue ! videoscale ! video/x-raw,width=1024,height=768,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=bilder/test.mp4 t. ! queue ! videorate ! video/x-raw,width=1024,height=768,framerate=1/1 ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg
@@ -9,4 +9,7 @@ gst-launch-1.0 tcamsrc num-buffers=40 serial=4810628 ! video/x-bayer,format=bggr
 
 #gst-launch-1.0 tcamsrc num-buffers=120 ! tee name=t t. ! video/x-raw,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=bilder/faen.mp4 t. ! videorate ! video/x-raw,framerate=1/1 ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg
 
-#gst-launch-1.0 tcamsrc num-buffers=10 ! tee name=t t. ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg t. ! videoconvert ! x264enc ! mp4mux ! filesink location=bilder/faen.mp4
+
+#gst-launch-1.0 tcamsrc name=src ! queue max_size_buffers=2 ! videoconvert ! capsfilter caps="video/x-raw,format=BGRx" ! videoconvert ! gtksink name=sink
+
+gst-launch-1.0 tcamsrc num-buffers=30 name=src ! tee name=t t. ! videoconvert ! jpegenc ! multifilesink location=bilder/frame_%04d.jpg t. ! videoconvert ! x264enc ! mp4mux ! filesink location=bilder/test.mp4
